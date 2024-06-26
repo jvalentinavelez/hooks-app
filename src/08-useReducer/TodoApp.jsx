e92cf2d9845ac6f3f1273f4757d04f6800813da8
Juanita Velez <jvalentina.velez@gmail.com>
2024-06-26 17:46:00 -0500

    Funcionalidad useReducer y persistencia mediante localStorage

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,27 +1,41 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar la piedra del alma",
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Recolectar la piedra del alma",
-    done: false,
-  },
+  //   {
+  //     id: new Date().getTime(),
+  //     description: "Recolectar la piedra del alma",
+  //     done: false,
+  //   },
+  //   {
+  //     id: new Date().getTime() * 3,
+  //     description: "Recolectar la piedra del alma",
+  //     done: false,
+  //   },
 ];
+
+const init = () => {
+  //si no hay datos en el localStorage devuelve un arreglo vacío
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
   //se pasa la referencia a función todoReducer, no se ejecuta directamente
-  const [todos, dispatchTodo] = useReducer(todoReducer, initialState);
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const handleNewTodo = (addTodo) => {
-    console.log(addTodo);
+    // console.log(addTodo);
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: addTodo,
+    };
+    dispatchTodo(action);
   };
 
   return (
